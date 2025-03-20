ebc7524027593e062d273ad4b901019d9784d355
anisafe2002 <anisafe2002@gmail.com>
2025-03-20 20:01:16 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -30,7 +30,7 @@ def create_weekday_df(df):
     sum_weekday_df = df.groupby("weekday_y").cnt_y.sum().sort_values(ascending=False).reset_index()
     return sum_weekday_df
 
-all_df = pd.read_csv("dashboard\all_data.csv")
+all_df = pd.read_csv("dashboard/all_data.csv")
 
 datetime_columns = ["dteday"]
 all_df.sort_values(by="dteday", inplace=True)
